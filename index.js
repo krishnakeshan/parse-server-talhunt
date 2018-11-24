5d559873b0420f73803017f9a2bdf68df9435bfe
Krishna Keshan <krishna@Krishnas-MacBook-Air.local>
2018-11-24 19:28:57 +0530

    now server starts after getting public ip

--- index.js
+++ index.js
@@ -5,59 +5,66 @@ var express = require('express');
 var ParseServer = require('parse-server').ParseServer;
 var ParseDashbord = require('parse-dashboard');
 var path = require('path');
+var publicIP = require("public-ip");
 
-var options = {
-  allowInsecureHTTP: true
-}
-
-var dashboard = new ParseDashbord({
-  "apps": [{
-    "serverURL": "http://localhost:1337/parse",
-    "appId": "talhuntAppId",
-    "masterKey": "ioVfggXfTl9NGww0Cc55",
-    "appName": "Talhunt"
-  }]
-}, options)
-
-var api = new ParseServer({
-  databaseURI: 'mongodb://localhost:27017/dev',
-  cloud: __dirname + '/cloud/main.js',
-  appId: 'talhuntAppId',
-  masterKey: 'ioVfggXfTl9NGww0Cc55', //Add your master key here. Keep it secret!
-  serverURL: 'http://localhost:1337/parse', // Don't forget to change to https if needed
-  liveQuery: {
-    classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
+//get machine's public ip address
+publicIP.v4().then(ip => {
+  var options = {
+    allowInsecureHTTP: true
   }
-});
-// Client-keys like the javascript key or the .NET key are not necessary with parse-server
-// If you wish you require them, you can set them as options in the initialization above:
-// javascriptKey, restAPIKey, dotNetKey, clientKey
 
-var app = express();
+  var dashboard = new ParseDashbord({
+    "apps": [{
+      "serverURL": "http://" + ip + ":1337/parse",
+      "appId": "talhuntAppId",
+      "masterKey": "ioVfggXfTl9NGww0Cc55",
+      "appName": "Talhunt"
+    }],
 
-// Serve static assets from the /public folder
-app.use('/public', express.static(path.join(__dirname, '/public')));
+    "users": [{
+      "user": "Manju",
+      "pass": "$2y$12$j3gPXg0FStkg2kFDATGMxurUj/3UYvILZZbvbyBaumf/Z7IE7BdNi"
+    }],
+    "useEncryptedPasswords": true
+  }, options)
 
-// Serve the Parse API on the /parse URL prefix
-app.use('/parse', api);
-app.use('/dashboard', dashboard);
+  var api = new ParseServer({
+    databaseURI: 'mongodb://localhost:27017/dev',
+    cloud: __dirname + '/cloud/main.js',
+    appId: 'talhuntAppId',
+    masterKey: 'ioVfggXfTl9NGww0Cc55',
+    serverURL: "http://" + ip + ":1337/parse", // Don't forget to change to https
+    liveQuery: {
+      classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
+    }
+  });
 
-// Parse Server plays nicely with the rest of your web routes
-app.get('/', function (req, res) {
-  res.status(200).send('I dream of being a website.  Please star the parse-server repo on GitHub!');
-});
+  var app = express();
 
-// There will be a test page available on the /test path of your server url
-// Remove this before launching your app
-app.get('/test', function (req, res) {
-  res.sendFile(path.join(__dirname, '/public/test.html'));
-});
+  // Serve static assets from the /public folder
+  app.use('/public', express.static(path.join(__dirname, '/public')));
 
-var port = 1337;
-var httpServer = require('http').createServer(app);
-httpServer.listen(port, function () {
-  console.log('parse-server-example running on port ' + port + '.');
-});
+  // Serve the Parse API on the /parse URL prefix
+  app.use('/parse', api);
+  app.use('/dashboard', dashboard);
 
-// This will enable the Live Query real-time server
-ParseServer.createLiveQueryServer(httpServer);
+  // Parse Server plays nicely with the rest of your web routes
+  app.get('/', function (req, res) {
+    res.status(200).send('Oh Hi There :)');
+  });
+
+  // There will be a test page available on the /test path of your server url
+  // Remove this before launching your app
+  app.get('/test', function (req, res) {
+    res.sendFile(path.join(__dirname, '/public/test.html'));
+  });
+
+  var port = 1337;
+  var httpServer = require('http').createServer(app);
+  httpServer.listen(port, function () {
+    console.log('parse-server-example running on port ' + port + '.');
+  });
+
+  // This will enable the Live Query real-time server
+  ParseServer.createLiveQueryServer(httpServer);
+})
