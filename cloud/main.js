1b5d64f6448131a3803af700f45c107761e8e289
Krishna Keshan <krishnakeshan18@gmail.com>
2018-12-04 19:01:12 +0530

    added "supportUser" method

--- cloud/main.js
+++ cloud/main.js
@@ -21,3 +21,24 @@ require("./auth")
 Parse.Cloud.define('hello', function(req, res) {
   res.success("hello defined");
 });
+
+//cloud function to let one user support another
+Parse.Cloud.define("supportUser", function(req, res) {
+  //get params
+  var params = req.params
+  var from = params.from
+  var to = params.to
+
+  //create a new Support object
+  var newSupportObject = new objects.SupportObject()
+  newSupportObject.set("from", from)
+  newSupportObject.set("to", to)
+  newSupportObject.save().then((savedSupportObject) => {
+    //saved support object
+    res.success("You are now supporting this user")
+  }, (error) => {
+    //error saving support object
+    console.log("error saving support object " + error)
+    res.error("There was an error supporting this user. Please try again in sometime.");
+  })
+})
--- cloud/objects.js
+++ cloud/objects.js
@@ -3,6 +3,7 @@ exports.facebookAPIBaseURL = "https://graph.accountkit.com/v1.3/me/"
 exports.facebookAppId = "2196060427333447"
 
 //Parse objects
+exports.SupportObject = Parse.Object.extend("Support")
 
 //options object for using master key
 exports.useMasterKeyOption = {
