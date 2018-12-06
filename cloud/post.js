d49030cbdaed50d7e6c0945ea035e5ff01c1e9aa
Krishna Keshan <krishnakeshan18@gmail.com>
2018-12-06 13:35:21 +0530

    added star method

--- cloud/post.js
+++ cloud/post.js
@@ -18,6 +18,8 @@ Parse.Cloud.define("createPost", function(req, res) {
     newPost.set("type", type)
     newPost.set("secondaryType", secondaryType)
     newPost.set("uuid", uuid)
+    newPost.set("stars", [])
+    newPost.set("starCount", 0)
     newPost.save(null, objects.useMasterKeyOption).then((savedPostObject) => {
         //saved post object
         res.success("Post Published")
@@ -25,5 +27,37 @@ Parse.Cloud.define("createPost", function(req, res) {
         //error saving post object
         console.log("error saving post object " + error)
         res.error("There was an error posting. Please try again in sometime.")
+    })
+})
+
+//function to star this post
+Parse.Cloud.define("starPost", function(req, res) {
+    //get params
+    var params = req.params
+    var postId = params.postId
+    var userId = params.userId
+
+    //get post object
+    var postQuery = new Parse.Query(objects.PostObject)
+    postQuery.get(postId, objects.useMasterKeyOption).then((postObject) => {
+        //got post object, add this user's star if not already added
+        if (!postObject.get("stars").includes(userId)) {
+            var stars = postObject.get("stars"), starCount = postObject.get("starCount")
+            stars.push(userId)
+            starCount += 1
+            postObject.set("stars", stars)
+            postObject.set("starCount", starCount)
+            postObject.save(null, objects.useMasterKeyOption).then((savedPost) => {
+                res.success("post saved")
+            }, (error) => {
+                //error saving post
+                console.log("error saving post " + error)
+                res.error("error saving post " + error)
+            })
+        }
+    }, (error) => {
+        //error getting post object
+        console.log("error getting post object " + error)
+        res.error("error getting post object " + error)
     })
 })
