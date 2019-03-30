f01ec8ae58f69699f0ad002d1b8290039aa21259
Krishna Keshan <krishnakeshan18@gmail.com>
2019-03-30 06:16:39 +0530

    added method to share post

--- cloud/post.js
+++ cloud/post.js
@@ -21,6 +21,7 @@ Parse.Cloud.define("createPost", function (req, res) {
     newPost.set("uuid", uuid)
     newPost.set("stars", [])
     newPost.set("starCount", 0)
+    newPost.set("isShared", false)
     newPost.save(null, objects.useMasterKeyOption).then((savedPostObject) => {
         //saved post object, now create recommendation object
         var newRecommendation = new objects.RecommendationObject()
@@ -134,16 +135,65 @@ Parse.Cloud.define("recommendPost", function (req, res) {
     })
 })
 
+//method to share post
+Parse.Cloud.define("sharePost", function (req, res) {
+    //get params
+    var params = req.params
+    var postId = params.postId
+    var userId = params.userId
+
+    //get post object
+    const postQuery = new Parse.Query(objects.PostObject)
+    postQuery.get(postId, objects.useMasterKeyOption).then((postObject) => {
+        //got post object, now copy it
+        var newPost = new objects.PostObject()
+        newPost.set("from", userId)
+        newPost.set("content", postObject.get("content"))
+        newPost.set("uuid", postObject.get("uuid"))
+        newPost.set("type", postObject.get("type"))
+        newPost.set("secondaryType", postObject.get("secondaryType"))
+        newPost.set("stars", postObject.get("stars"))
+        newPost.set("starCount", postObject.get("starCount"))
+        newPost.set("isShared", true)
+
+        //save it
+        newPost.save(null, objects.useMasterKeyOption).then((postObject) => {
+            //saved post object, return
+            console.log("shared post")
+            res.success(true)
+        }, (error) => {
+            //error saving post object
+            console.log("error saving post object")
+            res.error("error saving post object")
+        })
+    }, (error) => {
+        //error getting post object, log error
+        console.log("error getting post object " + error)
+        res.error("error getting post object")
+    })
+})
+
 //method to increment top recommendation for this post
-Parse.Cloud.define("incrementRecommendation", async (req) => {
+Parse.Cloud.define("incrementRecommendation", function (req, res) {
     //get params
     var params = req.params
     var recommendationId = params.recommendationId
     var userId = params.userId
 
     //get recommendation object
-    const recommendationQuery = Parse.Query("Recommendation")
-    var recommendationObject = await recommendationQuery.get(recommendationId)
+    const recommendationQuery = new Parse.Query(objects.RecommendationObject)
+    recommendationQuery.get(recommendationId, objects.useMasterKeyOption).then((recommendationObject) => {
+        //got recommendation object
+        //get list of supporters
+        var supporters = recommendationObject.get("support")
+        if (!supporters.includes(userId)) {
+            supporters.push(userId)
+        }
+    }, (error) => {
+        //error getting recommendation object
+        console.log("error getting recommendation object " + error)
+        res.error("error getting recommendation object")
+    })
 
     //get list of supporters
     var supporters = recommendationObject.get("support")
